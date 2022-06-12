8aa7b5d638e36aa46665a4a0523e559263843d6c
Charles <adeakinyemi129@gmail.com>
2022-06-12 16:09:54 +0300

    adjusted better management for private key

--- deploy.py
+++ deploy.py
@@ -1,7 +1,7 @@
 from solcx import compile_standard
 import json
 from web3 import Web3
-
+import os 
 
 
 with open("./SimpleStorage.sol", "r") as file:
@@ -32,9 +32,11 @@ abi = compiled_sol["contracts"]["simpleStorage.sol"]["SimpleStorage"]["abi"]
 
 #connecting ganache 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0xa2A6b33F4937A8EC3B618591Aa693810Eb01d9B0"
-my_privateKey = "0xed7f9aa35b2404a5739c9280aa42dfd23c0da5aa55ef0d6757786df89592bed6"
+chain_id = 1337
+my_address = "0x010ae82e8EF6dd16EE41c85A71f2D8E80B6eF3BA"
+#my_privateKey = "0xf83809eb85cacc3d9c09417a48286bb1a35045f18f3d93acbcb4ed36e4f87572"
+my_privateKey = os.getenv("PRIVATE_KEY")
+print(my_privateKey)
 
 #create the contract in python
 
@@ -42,4 +44,14 @@ SimpleStorage = w3.eth.contract(abi = abi, bytecode=bytecode)
 
 #get transactions count 
 nonce = w3.eth.getTransactionCount(my_address)
-print(nonce)
+
+#to create a transaction; a transaction occurs each time we change the state of our blockchain
+#1. Build a transaction 
+#2. Sign a transaction
+#3. send a transaction 
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"gasPrice": w3.eth.gas_price,"chainId": chain_id, "from": my_address, "nonce": nonce}
+    )
+
+signed_txn = w3.eth.account.signTransaction(transaction, private_key= my_privateKey)
