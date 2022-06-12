4c7aecd446f273c478e13bcb3f0b340b6826c152
Charles <adeakinyemi129@gmail.com>
2022-06-12 15:07:58 +0300

    nonce

--- deploy.py
+++ deploy.py
@@ -1,7 +1,7 @@
 from solcx import compile_standard
-
-# install_solc("0.6.0")
 import json
+from web3 import Web3
+
 
 
 with open("./SimpleStorage.sol", "r") as file:
@@ -30,4 +30,16 @@ bytecode = compiled_sol["contracts"]["simpleStorage.sol"]["SimpleStorage"]["evm"
 #get abi 
 abi = compiled_sol["contracts"]["simpleStorage.sol"]["SimpleStorage"]["abi"]
 
-print(abi)
+#connecting ganache 
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0xa2A6b33F4937A8EC3B618591Aa693810Eb01d9B0"
+my_privateKey = "0xed7f9aa35b2404a5739c9280aa42dfd23c0da5aa55ef0d6757786df89592bed6"
+
+#create the contract in python
+
+SimpleStorage = w3.eth.contract(abi = abi, bytecode=bytecode)
+
+#get transactions count 
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
