5c6e681d1254f4779ab996d575b17ec0f6eb21dc
Charles <adeakinyemi129@gmail.com>
2022-06-14 10:00:42 +0300

    several changes

--- deploy.py
+++ deploy.py
@@ -2,6 +2,9 @@ from solcx import compile_standard
 import json
 from web3 import Web3
 import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 with open("./SimpleStorage.sol", "r") as file:
@@ -30,11 +33,11 @@ bytecode = compiled_sol["contracts"]["simpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["simpleStorage.sol"]["SimpleStorage"]["abi"]
 
 #connecting ganache 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 1337
-my_address = "0xd28A43Dddc9AD965d49E015fEFB0B248614DcE2B"
+w3 = Web3(Web3.HTTPProvider("https://rinkeby.infura.io/v3/91030e0778df4696aa19e04d08b2d465"))
+chain_id = 4
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 #my_privateKey = "0xbb733f612a0bc6341f72a5dce9a7ee1906098746578eb0b2b30257d91aac8e9e"
-my_privateKey = os.getenv("PRIVATE_KEY")
+my_privateKey = os.getenv("PRIVATE_KEY1")
 
 #create the contract in python
 SimpleStorage = w3.eth.contract(abi = abi, bytecode=bytecode)
@@ -64,8 +67,17 @@ simple_storage = w3.eth.contract(address = txn_receipt.contractAddress, abi = ab
 print(simple_storage.functions.retrieve().call())
 
 store_txn = simple_storage.functions.store(12).buildTransaction(
-    {"gasPrice": w3.eth.gas_price,"chainId": chain_id, "from": my_address, "nonce": nonce}
+    {"gasPrice": w3.eth.gas_price,"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
     )
+
+signed_store_txn = w3.eth.account.signTransaction(store_txn, private_key = my_privateKey)
+
+print("Deploying contract...")
+send_store_txn = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+store_txn_receipt = w3.eth.wait_for_transaction_receipt(send_store_txn)
+print("Deployed !")
+
+print(simple_storage.functions.retrieve().call())
 
 
 
